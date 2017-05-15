d287283ae81b7939a9a669d6f1024c6d6dee6101
Brandon Ferguson <brfergus2@gmail.com>
2017-05-15 17:28:20 -0400

    blog index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
 end
