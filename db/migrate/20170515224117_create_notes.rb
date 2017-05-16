a4a40407aee43b127b05c221a29c4549fd112da8
Brandon Ferguson <brfergus2@gmail.com>
2017-05-16 09:36:55 -0400

    migration fix

--- db/migrate/20170515224117_create_notes.rb
+++ db/migrate/20170515224117_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
       t.references :user, foreign_key: true
-      t.references :product, foreign_key: true
+      t.references :post, foreign_key: true
       t.text :body
 
       t.timestamps
