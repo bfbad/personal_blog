5024d666e5fd3640647e7eb588964d10b9fb2239
Brandon Ferguson <brfergus2@gmail.com>
2017-05-16 09:29:58 -0400

    user login and comments

---
+++ db/migrate/20170515224117_create_notes.rb
@@ -0,0 +1,11 @@
+class CreateNotes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notes do |t|
+      t.references :user, foreign_key: true
+      t.references :product, foreign_key: true
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20170516003606_change_product_i_dto_post_id.rb
@@ -0,0 +1,5 @@
+class ChangeProductIDtoPostId < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :notes, :product_id, :post_id
+  end
+end
